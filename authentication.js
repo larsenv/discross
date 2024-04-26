13fb2f7defc844e9f5391e0473eeeaa7bc3dea48
Larsenv <larsen@weborican.com>
2024-04-26 19:13:53 +0000

    Simplify the cookies

--- authentication.js
+++ authentication.js
@@ -188,11 +188,11 @@ exports.handleLoginRegister = async function (req, res, body) {
       const result = await exports.login(params.username, params.password)
       if (result.status === 'success') {
         if (params.redirect) {
-          res.writeHead(200, { 'Set-Cookie': ['sessionID=deleted; expires=Thu, 01 Jan 1970 00:00:01 GMT; Max-Age=0; HttpOnly', 'sessionID=' + result.sessionID + '; Expires=' + new Date(result.expires * 1000) + '; path=/'], Location: params.redirect + '#end', 'Content-Type': 'text/html' })
-          res.write('<head><script>document.cookie = ' + "'" + 'sessionID =' + result.sessionID + '; Expires=' + new Date(result.expires * 1000) + '; path=/' + "'" + ';' + '</script>' + '<meta http-equiv="refresh" content="0; URL=' + he.encode(params.redirect) + '" />' + 'Logged in. Click <a href="' + he.encode(params.redirect) + '">here</a> to continue</head>')
+          res.writeHead(200, { 'Set-Cookie': ['sessionID=' + result.sessionID + '; Expires=' + new Date(result.expires * 1000) + '; path=/;'], Location: params.redirect + '#end', 'Content-Type': 'text/html' })
+          res.write('<head><meta http-equiv="refresh" content="0; URL=' + he.encode(params.redirect) + '" />' + 'Logged in. Click <a href="' + he.encode(params.redirect) + '">here</a> to continue</head>')
         } else {
-          res.writeHead(200, { 'Set-Cookie': ['sessionID=deleted; expires=Thu, 01 Jan 1970 00:00:01 GMT; Max-Age=0; HttpOnly', 'sessionID=' + result.sessionID + '; Expires=' + new Date(result.expires * 1000) + '; path=/'], Location: params.redirect + '#end', 'Content-Type': 'text/html' })
-          res.write('<head><script>document.cookie = ' + "'" + 'sessionID =' + result.sessionID + '; Expires=' + new Date(result.expires * 1000) + '; path=/' + "'" + ';' + '</script>' + '<meta http-equiv="refresh" content="0; URL=' + he.encode(params.redirect) + '" />' + 'Logged in. Click <a href="' + he.encode(params.redirect) + '">here</a> to continue</head>')
+          res.writeHead(200, { 'Set-Cookie': ['sessionID=' + result.sessionID + '; Expires=' + new Date(result.expires * 1000) + '; path=/;'], Location: '/serer/' + '#end', 'Content-Type': 'text/html' })
+          res.write('<head><meta http-equiv="refresh" content="0; URL=discross.rc24.xyz/server/" />' + 'Logged in. Click <a href="/server/">here</a> to continue</head>')
         }
         res.end()
       } else {
