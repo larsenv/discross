e3981dff24cb731cc9af43ce1b688a1544997953
Artem Nizhensky <inqurity@proton.me>
2024-08-19 16:58:51 +0300

    Remove leftover from testing

--- pages/server.js
+++ pages/server.js
@@ -60,7 +60,7 @@ exports.processServer = async function (bot, req, res, args, discordID) {
           cachedMembers[discordID][server.id] = member;
         }
         if (/*(isGuest && guestServers.includes(server.id)) ||*/ (member && member.user)) {
-         // fs.promises.writeFile(path.resolve(`pages/static/ico/server`, sanitizer(`${server.serverID}/${server.icon.startsWith("a_") ? server.icon.substring(2) : server.icon}.gif`)), await (await fetch(`https://cdn.discordapp.com/icons/${server.serverID}/${server.icon.startsWith("a_") ? server.icon.substring(2) : server.icon}.gif?size=128`)).arrayBuffer());
+         if (server.icon) fs.promises.writeFile(path.resolve(`pages/static/ico/server`, sanitizer(`${server.serverID}/${server.icon.startsWith("a_") ? server.icon.substring(2) : server.icon}.gif`)), await (await fetch(`https://cdn.discordapp.com/icons/${server.serverID}/${server.icon.startsWith("a_") ? server.icon.substring(2) : server.icon}.gif?size=128`)).arrayBuffer());
           serverHTML = strReplace(server_icon_template, "{$SERVER_ICON_URL}", server.icon ? `/ico/server/${server.id}/${server.icon.startsWith("a_") ? server.icon.substring(2) : server.icon}.gif` : "/discord-mascot.gif");
           serverHTML = strReplace(serverHTML, "{$SERVER_URL}", "./" + server.id);
           serverHTML = strReplace(serverHTML, "{$SERVER_NAME}", '"' + server.name + '"');
