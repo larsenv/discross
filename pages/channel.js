f8396fc71c0a528cad2804586a1a670da58d8391
Larsen Vallecillo <larsen@Weborican.com>
2024-09-26 11:32:30 -0500

    Revert

--- pages/channel.js
+++ pages/channel.js
@@ -237,7 +237,7 @@ exports.processChannel = async function processChannel(bot, req, res, args, disc
       
       const custom_emoji_matches = [...response.matchAll?.(/&lt;(:)?(?:(a):)?(\w{2,32}):(\d{17,19})?(?:(?!\1).)*&gt;?/g)];                // I'm not sure how to detect if an emoji is inline, since we don't have the whole message here to use it's length.
       if (custom_emoji_matches[0] && imagesCookie) custom_emoji_matches.forEach(async match => {                                                          // Tried Regex to find the whole message by matching the HTML tags that would appear before and after a message
-        response = response.replace(match[0], `<img src="/imageProxy/emoji/${match[4]}.${match[2] ? "gif" : "png"}" style="width: 6%;"  alt="emoji">`)    // Make it smaller if inline
+        response = response.replace(match[0], `<img src="/imageProxy/emoji/${match[4]}.${match[2] ? "gif" : "png"}" style="width: 3%;"  alt="emoji">`)    // Make it smaller if inline
       })
       final = strReplace(final, "{$MESSAGES}", response);
 
