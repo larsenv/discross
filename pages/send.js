a8630e59bd036445269d0118a0ca983a7c616ea9
Larsen Vallecillo <larsen@Weborican.com>
2024-11-08 14:08:20 -0600

    Add async lock for sending messages

--- pages/send.js
+++ pages/send.js
@@ -19,77 +19,84 @@ async function clean(server, nodelete) {
   });
 }
 
+const AsyncLock = require('async-lock');
+const lock = new AsyncLock(); // Create a new lock instance
+
 exports.sendMessage = async function sendMessage(bot, req, res, args, discordID) {
   try {
-    parsedurl = url.parse(req.url, true);
-    if (parsedurl.query.message != "") {
-      const channel = await bot.client.channels.fetch(parsedurl.query.channel);
+    // Lock the code execution for this discordID
+    await lock.acquire(discordID, async () => {
+      parsedurl = url.parse(req.url, true);
+      
+      if (parsedurl.query.message != "") {
+        const channel = await bot.client.channels.fetch(parsedurl.query.channel);
+        const member = await channel.guild.members.fetch(discordID);
+        const user = member.user;
+        let username = user.tag;
 
-      const member = await channel.guild.members.fetch(discordID);
-      const user = member.user;
-      let username = user.tag;
+        if (member.displayName != user.username) {
+          username = member.displayName + " (@" + user.tag + ")";
+        }
 
-      if (member.displayName != user.username) {
-        username = member.displayName + " (@" + user.tag + ")";
-      }
+        if (!member.permissionsIn(channel).has(discord.PermissionFlagsBits.SendMessages, true)) {
+          res.write("You don't have permission to do that!");
+          res.end();
+          return;
+        }
 
-      // if (user.permissionsIn(channel).FLAGS)]
-      if (!member.permissionsIn(channel).has(discord.PermissionFlagsBits.SendMessages, true)) { // True always allows admins to send messages
-        res.write("You don't have permission to do that!");
-        res.end();
-        return;
-      }
+        // Webhook handling
+        webhookDB = auth.dbQuerySingle("SELECT * FROM webhooks WHERE serverID=?", [channel.guild.id]);
 
-      // webhooks (serverID INT, webhookID INT, url STRING)
-      webhookDB = auth.dbQuerySingle("SELECT * FROM webhooks WHERE serverID=?", [channel.guild.id]);
-
-      if (!webhookDB) {
-        webhook = await channel.createWebhook({ name: "Discross", avatar: "pages/static/resources/logo.png", reason: "Discross uses webhooks to send messages" });
-        auth.dbQueryRun("INSERT INTO webhooks VALUES (?,?,?)", [channel.guild.id, webhook.id, webhook.token]);
-        clean(channel.guild, webhook.id); // Clean up all webhooks except the new one
-      } else {
-        // webhook = new Discord.WebhookClient(webhookDB.webhookID, webhookDB.token);
-        try {
-          webhook = await bot.client.fetchWebhook(webhookDB.webhookID);
-        } catch (err) {
+        let webhook;
+        if (!webhookDB) {
           webhook = await channel.createWebhook({ name: "Discross", avatar: "pages/static/resources/logo.png", reason: "Discross uses webhooks to send messages" });
           auth.dbQueryRun("INSERT INTO webhooks VALUES (?,?,?)", [channel.guild.id, webhook.id, webhook.token]);
           clean(channel.guild, webhook.id); // Clean up all webhooks except the new one
+        } else {
+          try {
+            webhook = await bot.client.fetchWebhook(webhookDB.webhookID);
+          } catch (err) {
+            webhook = await channel.createWebhook({ name: "Discross", avatar: "pages/static/resources/logo.png", reason: "Discross uses webhooks to send messages" });
+            auth.dbQueryRun("INSERT INTO webhooks VALUES (?,?,?)", [channel.guild.id, webhook.id, webhook.token]);
+            clean(channel.guild, webhook.id); // Clean up all webhooks except the new one
+          }
+          clean(channel.guild, webhookDB.webhookID);
         }
-        clean(channel.guild, webhookDB.webhookID);
+
+        let processedmessage = parsedurl.query.message;
+
+        // Regex to detect user mentions
+        var regex = /@([^#]{2,32}#\d{4})/g;
+        var m;
+
+        do {
+          m = regex.exec(processedmessage);
+          if (m) {
+            mentioneduser = await channel.guild.members.cache.find(member => member.user.tag == m[1]);
+            if (!mentioneduser) {
+              mentioneduser = (await channel.guild.members.fetch()).find(member => member.user.tag == m[1]);
+            }
+            if (mentioneduser) {
+              processedmessage = strReplace(processedmessage, m[0], "<@" + mentioneduser.id + ">");
+            }
+          }
+        } while (m);
+
+        await webhook.edit({ channel: channel });
+        const message = await webhook.send({
+          content: processedmessage,
+          username: username,
+          avatarURL: user.avatarURL(),
+          disableEveryone: true
+        });
+
+        bot.addToCache(message);
       }
 
-      processedmessage = parsedurl.query.message;
-
-      // https://stackoverflow.com/questions/6323417/regex-to-extract-all-matches-from-string-using-regexp-exec
-      // regex modified from https://www.reddit.com/r/discordapp/comments/6k4fml/username_requirements/
-
-      var regex = /@([^#]{2,32}#\d{4})/g; // Regular expression to detect user mentions
-      var m;
-
-      do {
-        m = regex.exec(processedmessage);
-        if (m) {
-          mentioneduser = await channel.guild.members.cache.find(member => member.user.tag == m[1]);
-          if (!mentioneduser) {
-            mentioneduser = (await channel.guild.members.fetch()).find(member => member.user.tag == m[1]);
-          }
-          if (mentioneduser) {
-            processedmessage = strReplace(processedmessage, m[0], "<@" + mentioneduser.id + ">");
-          }
-        }
-      } while (m);
-
-      await webhook.edit({ channel: channel });
-      message = await webhook.send({ content: processedmessage, username: username, avatarURL: user.avatarURL(), disableEveryone: true });
-
-      bot.addToCache(message);
-
-    }
-
-    res.writeHead(302, { "Location": "/channels/" + parsedurl.query.channel + "#end" });
-    res.end();
-  } catch {
+      res.writeHead(302, { "Location": "/channels/" + parsedurl.query.channel + "#end" });
+      res.end();
+    });
+  } catch (err) {
     res.writeHead(302, { "Location": "/server/" });
     res.end();
   }
