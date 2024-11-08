3468227093b065dc41b583f915098ecbb332ad93
Larsen Vallecillo <larsen@Weborican.com>
2024-11-08 15:02:55 -0600

    Fix send issues

--- pages/send.js
+++ pages/send.js
@@ -7,11 +7,9 @@ function strReplace(string, needle, replacement) {
   return string.split(needle).join(replacement || "");
 };
 
-// https://stackoverflow.com/questions/1967119/why-does-javascript-replace-only-first-instance-when-using-replace
-
 async function clean(server, nodelete) {
   (await server.fetchWebhooks()).forEach(async function (item) {
-    if ((item.owner.username.search("Discross") != -1) && (item.id != nodelete)) {
+    if ((item.owner.username.search("Discross") !== -1) && (item.id !== nodelete)) {
       try {
         await item.delete();
       } catch (err) { }
@@ -26,16 +24,15 @@ exports.sendMessage = async function sendMessage(bot, req, res, args, discordID)
   try {
     // Lock the code execution for this discordID
     await lock.acquire(discordID, async () => {
-      parsedurl = url.parse(req.url, true);
-      
-      if (parsedurl.query.message != "") {
+      const parsedurl = url.parse(req.url, true);
+      if (parsedurl.query.message !== "") {
         const channel = await bot.client.channels.fetch(parsedurl.query.channel);
         const member = await channel.guild.members.fetch(discordID);
         const user = member.user;
         let username = user.tag;
 
-        if (member.displayName != user.username) {
-          username = member.displayName + " (@" + user.tag + ")";
+        if (member.displayName !== user.username) {
+          username = `${member.displayName} (@${user.tag})`;
         }
 
         if (!member.permissionsIn(channel).has(discord.PermissionFlagsBits.SendMessages, true)) {
@@ -48,17 +45,26 @@ exports.sendMessage = async function sendMessage(bot, req, res, args, discordID)
         webhookDB = auth.dbQuerySingle("SELECT * FROM webhooks WHERE serverID=?", [channel.guild.id]);
 
         let webhook;
+
         if (!webhookDB) {
-          webhook = await channel.createWebhook({ name: "Discross", avatar: "pages/static/resources/logo.png", reason: "Discross uses webhooks to send messages" });
+          webhook = await channel.createWebhook({
+            name: "Discross",
+            avatar: "pages/static/resources/logo.png",
+            reason: "Discross uses webhooks to send messages"
+          });
           auth.dbQueryRun("INSERT INTO webhooks VALUES (?,?,?)", [channel.guild.id, webhook.id, webhook.token]);
           clean(channel.guild, webhook.id); // Clean up all webhooks except the new one
         } else {
           try {
             webhook = await bot.client.fetchWebhook(webhookDB.webhookID);
           } catch (err) {
-            webhook = await channel.createWebhook({ name: "Discross", avatar: "pages/static/resources/logo.png", reason: "Discross uses webhooks to send messages" });
+            webhook = await channel.createWebhook({
+              name: "Discross",
+              avatar: "pages/static/resources/logo.png",
+              reason: "Discross uses webhooks to send messages"
+            });
             auth.dbQueryRun("INSERT INTO webhooks VALUES (?,?,?)", [channel.guild.id, webhook.id, webhook.token]);
-            clean(channel.guild, webhook.id); // Clean up all webhooks except the new one
+            clean(channel.guild, webhook.id);
           }
           clean(channel.guild, webhookDB.webhookID);
         }
@@ -66,18 +72,17 @@ exports.sendMessage = async function sendMessage(bot, req, res, args, discordID)
         let processedmessage = parsedurl.query.message;
 
         // Regex to detect user mentions
-        var regex = /@([^#]{2,32}#\d{4})/g;
-        var m;
-
+        const regex = /@([^#]{2,32}#\d{4})/g;
+        let m;
         do {
           m = regex.exec(processedmessage);
           if (m) {
-            mentioneduser = await channel.guild.members.cache.find(member => member.user.tag == m[1]);
+            let mentioneduser = await channel.guild.members.cache.find(member => member.user.tag === m[1]);
             if (!mentioneduser) {
-              mentioneduser = (await channel.guild.members.fetch()).find(member => member.user.tag == m[1]);
+              mentioneduser = (await channel.guild.members.fetch()).find(member => member.user.tag === m[1]);
             }
             if (mentioneduser) {
-              processedmessage = strReplace(processedmessage, m[0], "<@" + mentioneduser.id + ">");
+              processedmessage = strReplace(processedmessage, m[0], `<@${mentioneduser.id}>`);
             }
           }
         } while (m);
@@ -86,18 +91,19 @@ exports.sendMessage = async function sendMessage(bot, req, res, args, discordID)
         const message = await webhook.send({
           content: processedmessage,
           username: username,
-          avatarURL: user.avatarURL(),
+          avatarURL: await user.avatarURL(),
           disableEveryone: true
         });
 
         bot.addToCache(message);
       }
 
-      res.writeHead(302, { "Location": "/channels/" + parsedurl.query.channel + "#end" });
+      res.writeHead(302, { "Location": `/channels/${parsedurl.query.channel}#end` });
       res.end();
     });
   } catch (err) {
+    console.error("Error processing message:", err);
     res.writeHead(302, { "Location": "/server/" });
     res.end();
   }
-}
+};
