561c7e8e8d07478e7d9706869c9ac9cdec301b6e
larsenv <larsen@Weborican.com>
2026-01-25 20:09:32 -0600

    Refactor HTML template loading without minification
    Removed minification of HTML templates for easier editing and added new templates for input and file download.

--- pages/draw.js
+++ pages/draw.js
@@ -16,20 +16,19 @@ const fetch = require("sync-fetch");
 // Is that a bad idea?
 
 // Templates for viewing the messages in a channel
-const channel_template = minifier.htmlMinify(fs.readFileSync('pages/templates/draw.html', 'utf-8'));
+const channel_template = fs.readFileSync('pages/templates/draw.html', 'utf-8');
 
+const message_template = fs.readFileSync('pages/templates/message/message.html', 'utf-8');
+const first_message_content_template = fs.readFileSync('pages/templates/message/first_message_content.html', 'utf-8');
+const merged_message_content_template = fs.readFileSync('pages/templates/message/merged_message_content.html', 'utf-8');
+const mention_template = fs.readFileSync('pages/templates/message/mention.html', 'utf-8');
 
-const message_template = minifier.htmlMinify(fs.readFileSync('pages/templates/message/message.html', 'utf-8'));
-const first_message_content_template = minifier.htmlMinify(fs.readFileSync('pages/templates/message/first_message_content.html', 'utf-8'));
-const merged_message_content_template = minifier.htmlMinify(fs.readFileSync('pages/templates/message/merged_message_content.html', 'utf-8'));
-const mention_template = minifier.htmlMinify(fs.readFileSync('pages/templates/message/mention.html', 'utf-8'));
+const input_template = fs.readFileSync('pages/templates/channel/input.html', 'utf-8');
+const input_disabled_template = fs.readFileSync('pages/templates/channel/input_disabled.html', 'utf-8');
 
-const input_template = minifier.htmlMinify(fs.readFileSync('pages/templates/channel/input.html', 'utf-8'));
-const input_disabled_template = minifier.htmlMinify(fs.readFileSync('pages/templates/channel/input_disabled.html', 'utf-8'));
+const no_message_history_template = fs.readFileSync('pages/templates/channel/no_message_history.html', 'utf-8');
 
-const no_message_history_template = minifier.htmlMinify(fs.readFileSync('pages/templates/channel/no_message_history.html', 'utf-8'));
-
-const file_download_template = minifier.htmlMinify(fs.readFileSync('pages/templates/channel/file_download.html', 'utf-8'));
+const file_download_template = fs.readFileSync('pages/templates/channel/file_download.html', 'utf-8');
 
 function strReplace(string, needle, replacement) {
   return string.split(needle).join(replacement || "");
