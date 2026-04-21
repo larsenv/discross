7b03cb4b6a376ac6da078dfcb3515c74a914b0dd
larsenv <larsen@Weborican.com>
2026-04-21 15:16:30 -0500

    Add credit

---
+++ pages/credits.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+    renderTemplate,
+    getPageThemeAttr,
+    loadAndRenderPageTemplate,
+    getTemplate,
+} = require('./utils.js');
+const escape = require('escape-html');
+const auth = require('../authentication.js');
+
+const credits_template = loadAndRenderPageTemplate('credits');
+
+const logged_in_template = getTemplate('logged_in', 'index');
+const logged_out_template = getTemplate('logged_out', 'index');
+
+exports.processCredits = async function (bot, req, res, args) {
+    const discordID = await auth.checkAuth(req, res, true); // true means that the user isn't redirected to the login page
+    const menuOptions = discordID
+        ? renderTemplate(logged_in_template, { USER: escape(await auth.getUsername(discordID)) })
+        : logged_out_template;
+    const response = renderTemplate(credits_template, {
+        MENU_OPTIONS: menuOptions,
+        WHITE_THEME_ENABLED: getPageThemeAttr(req),
+    });
+    res.end(response);
+};
